5560aa37120ab52d010e699a850a1e9573f7673b
Matheus <matheusaraujo.dev@gmail.com>
2023-05-19 13:41:36 -0300

    Add: layout and NewMemory pages (mobile)

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,42 +1,25 @@
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
-} from '@expo-google-fonts/roboto'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { useRouter } from 'expo-router'
 import * as SecureStore from 'expo-secure-store'
-import { StatusBar } from 'expo-status-bar'
-import { styled } from 'nativewind'
 import { useEffect } from 'react'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-import blurBg from '../src/assets/bg-blur.png'
-import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import Stripes from '../src/assets/stripes.svg'
-import { api } from '../src/lib/api'
+import { Text, TouchableOpacity, View } from 'react-native'
 
-const StyledStripes = styled(Stripes)
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
   tokenEndpoint: 'https://github.com/login/oauth/access_token',
   revocationEndpoint:
-    'https://github.com/settings/connections/applications/fcf5ace4f93d421c4fbb',
+    'https://github.com/settings/connections/applications/d26f194cc5d5132a51be',
 }
 
 export default function App() {
   const router = useRouter()
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
-
-  const [, response, singInWithGitHub] = useAuthRequest(
+  const [, response, signInWithGithub] = useAuthRequest(
     {
-      clientId: 'fcf5ace4f93d421c4fbb',
+      clientId: 'd26f194cc5d5132a51be',
       scopes: ['identity'],
       redirectUri: makeRedirectUri({
         scheme: 'nlwspacetime',
@@ -45,7 +28,7 @@ export default function App() {
     discovery,
   )
 
-  async function handleGitHub0AuthCode(code: string) {
+  async function handleGithubOAuthCode(code: string) {
     const response = await api.post('/register', {
       code,
     })
@@ -53,28 +36,27 @@ export default function App() {
     const { token } = response.data
 
     await SecureStore.setItemAsync('token', token)
+
     router.push('/memories')
   }
 
   useEffect(() => {
+    // console.log(
+    //   'response',
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
     if (response?.type === 'success') {
       const { code } = response.params
-      handleGitHub0AuthCode(code)
+
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -91,7 +73,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
-          onPress={() => singInWithGitHub()}
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
@@ -102,8 +84,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
